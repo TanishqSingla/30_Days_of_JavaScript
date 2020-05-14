7b6f36eeb1060ff81c8fb90789aeb97fc5ad6891
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-15 02:23:42 +0530

    Moved second hand

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -1,5 +1,11 @@
+const secondHand = document.querySelector(".second-hand");
+
 function setDate() {
-  console.log("hi");
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90; //why +90?, Well to compensate the offset
+
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
