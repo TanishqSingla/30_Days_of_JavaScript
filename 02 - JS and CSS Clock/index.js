656d99e6a2da29e392b27666af25bb769bfa8a0d
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-15 02:33:04 +0530

    Completed JS Clock

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -1,5 +1,6 @@
 const secondHand = document.querySelector(".second-hand");
 const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 function setDate() {
   const now = new Date();
@@ -12,6 +13,11 @@ function setDate() {
   const minDegrees = (min / 60) * 360 + 90;
 
   minHand.style.transform = `rotate(${minDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = (hour / 12) * 360 + 90;
+
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
