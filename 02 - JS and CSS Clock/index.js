8335aa5a727666b79e4398bc3b68263b94639cd3
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-15 02:39:42 +0530

    Fixed annoying clock twitching

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -7,17 +7,33 @@ function setDate() {
   const seconds = now.getSeconds();
   const secondsDegrees = (seconds / 60) * 360 + 90; //why +90?, Well to compensate the offset
 
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
   const min = now.getMinutes();
   const minDegrees = (min / 60) * 360 + 90;
-
-  minHand.style.transform = `rotate(${minDegrees}deg)`;
 
   const hour = now.getHours();
   const hourDegrees = (hour / 12) * 360 + 90;
 
-  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  if (seconds >= 59) {
+    secondHand.style.transform = `rotate(${secondsDegrees - 1}deg)`;
+    secondHand.style.transition = "none";
+  } else {
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  }
+
+  if (mins >= 59) {
+    minHand.style.transform = `rotate(${minsDegrees - 1}deg)`;
+    minHand.style.transition = "none";
+  } else {
+    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+  }
+  //minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  if (hours >= 59) {
+    hourHand.style.transform = `rotate(${hoursDegrees - 1}deg)`;
+    hourHand.style.transition = "none";
+  } else {
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  }
 }
 
 setInterval(setDate, 1000);
