1f6b70308092cd970891583f1a22b5c7fc55e1ef
TanishqSingla <tanishqsingla00@gmail.com>
2020-06-03 17:29:28 +0530

    added range buttons functionality:

--- 11 - Custom HTML5 video player/script.js
+++ 11 - Custom HTML5 video player/script.js
@@ -23,6 +23,10 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
 //Hooking function
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
@@ -31,3 +35,8 @@ video.addEventListener("pause", updateButton);
 toggle.addEventListener("click", togglePlay);
 
 skipButtons.forEach((btn) => btn.addEventListener("click", skip));
+
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
