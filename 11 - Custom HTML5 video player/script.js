540136ef16d68a989341ee425f6c4b538d156fee
TanishqSingla <tanishqsingla00@gmail.com>
2020-06-03 17:46:45 +0530

    Added click scrub

--- 11 - Custom HTML5 video player/script.js
+++ 11 - Custom HTML5 video player/script.js
@@ -32,6 +32,12 @@ function handleProgress() {
   progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+
+  video.currentTime = scrubTime;
+}
+
 //Hooking function
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
@@ -46,3 +52,5 @@ ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
 ranges.forEach((range) =>
   range.addEventListener("mousemove", handleRangeUpdate)
 );
+
+progress.addEventListener("click", scrub);
